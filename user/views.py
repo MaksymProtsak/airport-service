1ce64eeaa08343c5a0a258fd72ae66dd8e7e84b5
Maksym Protsak <maksym.protsak@gmail.com>
2024-11-28 07:13:38 +0200

    Created CreateUserView, CreateTokenView, ManageUserView.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        """Created a new user"""
+        return self.create(request, *args, **kwargs)
+
+
+class CreateTokenView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+    def post(self, request, *args, **kwargs):
+        """Login endpoint"""
+        return super().post(request, *args, **kwargs)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+    def get(self, request, *args, **kwargs):
+        """Retrieve information about current user"""
+        return super().get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        """Update all information about current users"""
+        return super().update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        """Update partial information about current users"""
+        return super().partial_update(request, *args, **kwargs)
