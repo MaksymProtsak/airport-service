426e0d239aa3ac7de0b600c7573f0ac60b009207
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-28 08:02:51 +0200

    Added permission_classes field in CreateUserView. Now any user can make registration.

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import AllowAny
 from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer, AuthTokenSerializer
@@ -7,6 +8,7 @@ from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
         """Created a new user"""
