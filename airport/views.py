3c24147024ddfbdb83fd7b280e94c7395dea5532
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-28 08:46:03 +0200

    Created CrewViewSet, AirplaneTypeViewSet.

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from airport.models import (
+    Crew,
+    AirplaneType,
+)
+from airport.serializer import (
+    CrewSerializer,
+    AirplaneTypeSerializer,
+)
+
+
+class CrewViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of crews"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new crew"""
+        return super().create(request, *args, **kwargs)
+
+
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of airplane types"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new airplane type"""
+        return super().create(request, *args, **kwargs)
