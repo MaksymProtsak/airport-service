c1b2301cf62c0d659ff76414c4da2d4b664c959d
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-11-29 08:48:59 +0200

    Created OrderViewSet, AirplaneViewSet, AirportViewSet, RouteViewSet, FlightViewSet, TicketViewSet.

--- airport/views.py
+++ airport/views.py
@@ -5,10 +5,22 @@ from rest_framework.viewsets import GenericViewSet
 from airport.models import (
     Crew,
     AirplaneType,
+    Order,
+    Airport,
+    Airplane,
+    Flight,
+    Route,
+    Ticket,
 )
 from airport.serializers import (
     CrewSerializer,
     AirplaneTypeSerializer,
+    OrderSerializer,
+    AirportSerializer,
+    AirplaneSerializer,
+    RouteSerializer,
+    FlightSerializer,
+    TicketSerializer,
 )
 
 
@@ -43,4 +55,106 @@ class AirplaneTypeViewSet(
 
     def create(self, request, *args, **kwargs):
         """Create a new airplane type"""
+        return super().create(request, *args, **kwargs)
+
+
+class OrderViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of orders"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new order"""
+        return super().create(request, *args, **kwargs)
+
+
+class AirplaneViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of airplanes"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new irplane"""
+        return super().create(request, *args, **kwargs)
+
+
+class AirportViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of airports"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new airport"""
+        return super().create(request, *args, **kwargs)
+
+
+class RouteViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of routes"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new route"""
+        return super().create(request, *args, **kwargs)
+
+
+class FlightViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of flights"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new flight"""
+        return super().create(request, *args, **kwargs)
+
+
+class TicketViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+    def list(self, request, *args, **kwargs):
+        """Retrieve list of tickets"""
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        """Create a new ticket"""
         return super().create(request, *args, **kwargs)
