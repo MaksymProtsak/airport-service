17174e643192104b703c8cec4b4a1dcdee904cc2
Maksym Protsak <maksym.protsak@gmail.com>
2024-12-04 14:22:50 +0200

    Created base.py.

--- airport_service/settings/base.py
+++ airport_service/settings/base.py
@@ -15,23 +15,25 @@ from pathlib import Path
 
 from datetime import timedelta
 
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-53)ivlyjs&$r=#9*5fst1&9sl(l4y_yu31do42k_tz1v7m!h*8'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "default not secure key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+# Assets Management
+ASSETS_ROOT = "/static/assets"
 
 # Application definition
 
@@ -80,20 +82,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'airport_service.wsgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
