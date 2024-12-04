62444283ecd2337031c53f0ab9440b4e5bf0307d
Maksym Protsak <112944888+MaksymProtsak@users.noreply.github.com>
2024-12-04 07:32:08 +0200

    Created SIMPLE_JWT with ACCESS_TOKEN_LIFETIME and REFRESH_TOKEN_LIFETIME.

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from datetime import timedelta
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,4 +156,9 @@ REST_FRAMEWORK = {
         "user": "30/min"
     }
 
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
